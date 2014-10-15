c9e0f9d3283f9865116d912b682ea809087375c7
nlsltz <info@nilslutz.de>
2014-10-15 13:45:01 +0200

    added component and util functions
    neue Datei:             Component.js
    neue Datei:             util/Formatter.js
    neue Datei:             util/Grouper.js

---
+++ util/Formatter.js
@@ -0,0 +1,37 @@
+jQuery.sap.declare("abat.Mockup.util.Formatter");
+
+jQuery.sap.require("sap.ui.core.format.DateFormat");
+
+abat.Mockup.util.Formatter = {
+	
+	_statusStateMap : {
+		"Neu" : "Error",
+		"Initial" : "Success"
+	},
+	
+	statusState :  function (value) {
+		var map = abat.Mockup.util.Formatter._statusStateMap;
+		return (value && map[value]) ? map[value] : "None";
+	},
+	
+	numberState :  function (stock, minStock) {
+		return (parseInt(stock) <= parseInt(minStock)) ? "Error" : "None";
+	},
+	
+	date : function (value) {
+		if (value) {
+			var oDateFormat = sap.ui.core.format.DateFormat.getDateTimeInstance({pattern: "yyyy-MM-dd"}); 
+			return oDateFormat.format(new Date(value));
+		} else {
+			return value;
+		}
+	},
+	
+	quantity :  function (value) {
+		try {
+			return (value) ? parseFloat(value).toFixed(0) : value;
+		} catch (err) {
+			return "Not-A-Number";
+		}
+	}
+};
