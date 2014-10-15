4a676574a8bbf03ca217067267ee25b797f69cce
nlsltz <info@nilslutz.de>
2014-10-15 13:49:13 +0200

    added model and view files
    neue Datei:             model/mock.json
    neue Datei:             view/App.controller.js
    neue Datei:             view/App.view.js
    neue Datei:             view/Detail.controller.js
    neue Datei:             view/Detail.view.xml
    neue Datei:             view/Empty.view.xml
    neue Datei:             view/ForecastChart.fragment.xml
    neue Datei:             view/Master.controller.js
    neue Datei:             view/Master.view.xml
    neue Datei:             view/OrderProposalChart.fragment.xml

---
+++ view/App.view.js
@@ -0,0 +1,31 @@
+sap.ui.jsview("abat.Mockup.view.App", {
+
+	getControllerName: function () {
+		return "abat.Mockup.view.App";
+	},
+	
+	createContent: function (oController) {
+		
+		// to avoid scroll bars on desktop the root view must be set to block display
+		this.setDisplayBlock(true);
+		
+		// create app
+		this.app = new sap.m.SplitApp();
+
+		// load the master page
+		var master = sap.ui.xmlview("Master", "abat.Mockup.view.Master");
+		master.getController().nav = this.getController();
+		this.app.addPage(master, true);
+		
+		// load the empty page
+		var empty = sap.ui.xmlview("Empty", "abat.Mockup.view.Empty");
+		this.app.addPage(empty, false);
+		
+		// wrap app with shell
+		return new sap.m.Shell("Shell", {
+			title : "{i18n>ShellTitle}",
+			showLogout : false,
+			app : this.app
+		});
+	}
+});
